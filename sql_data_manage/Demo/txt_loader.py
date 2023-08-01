ad72a5d1e82d5586077449d1dbe622ba5a92e1a5
chLi <565353780@qq.com>
2023-08-01 20:05:57 +0800

    finish loading txt data into structural list data format

--- sql_data_manage/Demo/txt_loader.py
+++ sql_data_manage/Demo/txt_loader.py
@@ -3,8 +3,9 @@ from sql_data_manage.Module.txt_loader import TXTLoader
 
 def demo():
     txt_file_path = "/home/chli/chLi/Sql-Dataset/text/dat_order.txt"
+    remove_quotes = True
 
-    txt_loader = TXTLoader(txt_file_path)
+    txt_loader = TXTLoader(txt_file_path, remove_quotes)
 
     print(txt_loader.title_list)
     print(len(txt_loader.data_list_list))
--- sql_data_manage/Module/txt_loader.py
+++ sql_data_manage/Module/txt_loader.py
@@ -1,14 +1,16 @@
 import os
 from tqdm import tqdm
 
+from sql_data_manage.Method.io import getLineNum, splitLineData
+
 
 class TXTLoader(object):
-    def __init__(self, txt_file_path=None):
+    def __init__(self, txt_file_path=None, remove_quotes=False):
         self.title_list = []
         self.data_list_list = []
 
         if txt_file_path is not None:
-            self.loadFile(txt_file_path)
+            self.loadFile(txt_file_path, remove_quotes)
         return
 
     def reset(self):
@@ -16,13 +18,38 @@ class TXTLoader(object):
         self.data_list_list = []
         return True
 
-    def loadFile(self, txt_file_path):
+    def loadFile(self, txt_file_path, remove_quotes=False):
         if not os.path.exists(txt_file_path):
             print('[ERROR][TXTLoader::loadFile]')
             print('\t txt file not exist!')
             print('\t', txt_file_path)
 
+        data_num = getLineNum(txt_file_path) - 1
+
+        invalid_data_num = 0
+
+        print('[INFO][TXTLoader::loadFile]')
+        print('\t start load sql txt data...')
         with open(txt_file_path, 'r', encoding='utf-8') as f:
-            data = f.readline()
-            print(data)
+            title = f.readline()
+
+            self.title_list = splitLineData(title, remove_quotes)
+
+            for _ in tqdm(range(data_num)):
+                data = f.readline()
+
+                if data == '\n':
+                    continue
+
+                data_list = splitLineData(data, remove_quotes)
+
+                if len(data_list) != len(self.title_list):
+                    invalid_data_num += 1
+                    continue
+
+                self.data_list_list.append(data_list)
+
+        if invalid_data_num > 0:
+            print('[WARN][TXTLoader::loadFile]')
+            print('\t found', invalid_data_num, 'invalid data!')
         return True
