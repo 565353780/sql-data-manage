00c5e05b124ab1bb16f30b7e0ad3bd61a0073b09
chLi <565353780@qq.com>
2023-08-03 14:27:19 +0800

    activate all functions, now will run not too slowly

--- demo.py
+++ demo.py
@@ -3,6 +3,6 @@ from sql_data_manage.Demo.data_matcher import demo as demo_match_data
 from sql_data_manage.Demo.prompt_generator import demo as demo_generate_prompt
 
 if __name__ == '__main__':
-    # demo_load_txt()
-    # demo_match_data()
+    demo_load_txt()
+    demo_match_data()
     demo_generate_prompt()
